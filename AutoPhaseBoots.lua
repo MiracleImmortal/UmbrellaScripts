89386188f8cef34fd4f86fa5ab265b5692b4cb5a
Valentin <shok@cpan.org>
2018-05-31 19:59:37 +0300

    Add timer

--- AutoPhaseBoots.lua
+++ AutoPhaseBoots.lua
@@ -2,8 +2,16 @@ local PhaseBoots = {}
 
 PhaseBoots.Enable = Menu.AddOptionBool({"Utility", "AutoUse"}, "PhaseBoots", false)
 
+PhaseBoots.LastUpdateTime = 0
+PhaseBoots.UpdateTime = 0.25
+
 function PhaseBoots.OnUpdate()
     if not Menu.IsEnabled(PhaseBoots.Enable) then return end
+
+    if ((os.clock() - PhaseBoots.LastUpdateTime) < PhaseBoots.UpdateTime) then
+        return
+    end
+    PhaseBoots.LastUpdateTime = os.clock();
 
     local hero = Heroes.GetLocal()
     if not hero or not Entity.IsAlive(hero) then return end
