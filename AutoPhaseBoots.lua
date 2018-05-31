6901c5db3d8d580a5d5c35a9d9e0c057dba8881b
Valentin <shok@cpan.org>
2018-05-31 19:17:43 +0300

    Check phaseBoots is not nil

--- AutoPhaseBoots.lua
+++ AutoPhaseBoots.lua
@@ -6,7 +6,7 @@ function PhaseBoots.OnUpdate()
     local hero = Heroes.GetLocal()
     if hero and Entity.IsAlive(hero) then
         local phaseBoots = NPC.GetItem(hero, "item_phase_boots")
-        if Menu.IsEnabled(PhaseBoots.Enable) and NPC.IsRunning(hero) and Ability.IsCastable(phaseBoots, NPC.GetMana(hero)) then
+        if phaseBoots and Menu.IsEnabled(PhaseBoots.Enable) and NPC.IsRunning(hero) and Ability.IsCastable(phaseBoots, NPC.GetMana(hero)) then
             Ability.CastNoTarget(phaseBoots)
         end
     end
