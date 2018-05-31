7149b6fe4c77d2ee90a1d2d8844d41115cf4c741
Valentin <shok@cpan.org>
2018-05-31 19:39:14 +0300

    Check nil for all values

--- AutoPhaseBoots.lua
+++ AutoPhaseBoots.lua
@@ -3,12 +3,19 @@ local PhaseBoots = {}
 PhaseBoots.Enable = Menu.AddOptionBool({"Utility", "AutoUse"}, "PhaseBoots", false)
 
 function PhaseBoots.OnUpdate()
+    if not Menu.IsEnabled(PhaseBoots.Enable) then return end
+
     local hero = Heroes.GetLocal()
-    if hero and Entity.IsAlive(hero) then
-        local phaseBoots = NPC.GetItem(hero, "item_phase_boots")
-        if phaseBoots and Menu.IsEnabled(PhaseBoots.Enable) and NPC.IsRunning(hero) and Ability.IsCastable(phaseBoots, NPC.GetMana(hero)) then
-            Ability.CastNoTarget(phaseBoots)
-        end
+    if not hero or not Entity.IsAlive(hero) then return end
+
+    local phaseBoots = NPC.GetItem(hero, "item_phase_boots")
+    if not phaseBoots then return end
+
+    local mana = NPC.GetMana(hero)
+    if mana == nil then return end
+
+    if NPC.IsRunning(hero) and Ability.IsCastable(phaseBoots, mana) then
+        Ability.CastNoTarget(phaseBoots)
     end
 end
 
