d59289fa93393e865af2a5a798a7e224aff0bad6
Valentin <shok@cpan.org>
2018-06-03 03:43:35 +0300

    Update WtfBrist.lua

--- WtfBrist.lua
+++ WtfBrist.lua
@@ -2,10 +2,15 @@ local wtfBrist = {}
 
 wtfBrist.IsEnable = Menu.AddOptionBool({"Hero Specific", "WTF+ Bristleback"}, "Enable", false)
 wtfBrist.SprayKey = Menu.AddKeyOption({"Hero Specific", "WTF+ Bristleback"}, "Spray Key", Enum.ButtonCode.KEY_1);
-wtfBrist.Debug = false
+wtfBrist.Debug = true
 
 wtfBrist.LastUpdateTime = 0
 wtfBrist.UpdateTime = 0.0
+
+wtfBrist.WarPathLevel         = 0
+wtfBrist.WarPathCounter       = 0
+wtfBrist.MaxWarPathCounter    = 0
+wtfBrist.WarPathBasicCounter  = 3
 
 function wtfBrist.OnUpdate()
     if not Menu.IsEnabled(wtfBrist.IsEnable) or not Engine.IsInGame() or not Heroes.GetLocal() then return end
@@ -16,34 +21,93 @@ function wtfBrist.OnUpdate()
     wtfBrist.LastUpdateTime = os.clock();
 
     wtfBrist.MyHero = Heroes.GetLocal()
-    if NPC.GetUnitName(wtfBrist.MyHero) ~= "npc_dota_hero_bristleback" then return end
-    if not Entity.IsAlive(wtfBrist.MyHero) or NPC.IsStunned(wtfBrist.MyHero) or NPC.IsSilenced(wtfBrist.MyHero) then return end
+    if not wtfBrist.MyHero or NPC.GetUnitName(wtfBrist.MyHero) ~= "npc_dota_hero_bristleback" then return end
 
+    local warPath = NPC.GetAbility(wtfBrist.MyHero, "bristleback_warpath")
+    if warPath and Ability.GetLevel(warPath) > wtfBrist.WarPathLevel then
+        wtfBrist.WarPathLevel = Ability.GetLevel(warPath)
+
+        if wtfBrist.WarPathLevel > 0 then
+            wtfBrist.MaxWarPathCounter = (wtfBrist.WarPathBasicCounter + (wtfBrist.WarPathLevel * 2))
+        else
+            wtfBrist.MaxWarPathCounter = 0
+        end
+    end
+
+    if not Entity.IsAlive(wtfBrist.MyHero) or NPC.IsStunned(wtfBrist.MyHero) or NPC.IsSilenced(wtfBrist.MyHero) then return end
     local quillSpray = NPC.GetAbility(wtfBrist.MyHero, "bristleback_quill_spray")
     if not quillSpray then return end
 
     local quillSprayCastRange = Ability.GetCastRange(quillSpray)
 
-    local Units = Entity.GetUnitsInRadius(wtfBrist.MyHero, quillSprayCastRange, Enum.TeamType.TEAM_ENEMY)
-    if not Units then return end
-
-    if not NPC.IsVisibleToEnemies(wtfBrist.MyHero) and not Menu.IsKeyDown(wtfBrist.SprayKey) then
+    if wtfBrist.IsHeroInvisible(wtfBrist.MyHero) and not Menu.IsKeyDown(wtfBrist.SprayKey) then
+        return
+    elseif Menu.IsKeyDown(wtfBrist.SprayKey) then
+        Ability.CastNoTarget(quillSpray)
+        return
+    elseif not wtfBrist.IsHeroInvisible(wtfBrist.MyHero) and wtfBrist.WarPathCounter < wtfBrist.MaxWarPathCounter then
+        Ability.CastNoTarget(quillSpray)
         return
     end
+
+    local Units = Entity.GetUnitsInRadius(wtfBrist.MyHero, quillSprayCastRange, Enum.TeamType.TEAM_ENEMY)
+    if not Units then return end
 
     for i, unit in pairs(Units) do
         if unit ~= nil and unit ~= 0 and NPCs.Contains(unit) and NPC.IsEntityInRange(wtfBrist.MyHero, unit, quillSprayCastRange) then
             if not Entity.IsSameTeam(unit, wtfBrist.MyHero) and not NPC.IsStructure(unit) and NPC.GetUnitName(unit) ~= "npc_dota_neutral_caster" then
-                if Entity.IsAlive(unit) and not NPC.IsWaitingToSpawn(unit) and not Entity.IsDormant(unit) and Ability.IsReady(quillSpray) then
-                    if NPC.IsCreep(unit) or NPC.IsIllusion(unit) or NPC.IsHero(unit) or NPC.IsCourier(unit) then
-                        if wtfBrist.Debug then Log.Write("Attack! [".. NPC.GetUnitName(unit) .."]") end
-                        Ability.CastNoTarget(quillSpray)
-                        break
+                if NPC.GetUnitName(unit) ~= "npc_dota_observer_wards" then
+                    if Entity.IsAlive(unit) and not NPC.IsWaitingToSpawn(unit) and not Entity.IsDormant(unit) and Ability.IsReady(quillSpray) then
+                        if NPC.IsCreep(unit) or NPC.IsIllusion(unit) or NPC.IsHero(unit) or NPC.IsCourier(unit) then
+                            if wtfBrist.Debug then Log.Write("Attack! [".. NPC.GetUnitName(unit) .."]") end
+                            Ability.CastNoTarget(quillSpray)
+                            break
+                        end
                     end
                 end
             end
         end
     end
+end
+
+function wtfBrist.OnModifierCreate(ent, xMod)
+    if ent == wtfBrist.MyHero and Modifier.GetName(xMod) == "modifier_bristleback_warpath_stack" then
+        wtfBrist.WarPathCounter = wtfBrist.WarPathCounter + 1
+        Log.Write("Create xMod -> "..Modifier.GetName(xMod).." -> "..wtfBrist.WarPathCounter)
+    end
+end
+
+function wtfBrist.OnModifierDestroy(ent, xMod)
+    if ent == wtfBrist.MyHero and Modifier.GetName(xMod) == "modifier_bristleback_warpath_stack" then
+        wtfBrist.WarPathCounter = wtfBrist.WarPathCounter - 1
+        Log.Write("Destroy xMod -> "..Modifier.GetName(xMod).." -> "..wtfBrist.WarPathCounter)
+    end
+end
+
+function wtfBrist.IsHeroInvisible(myHero)
+  if not myHero then return false end
+  if not Entity.IsAlive(myHero) then return false end
+
+  if NPC.HasState(myHero, Enum.ModifierState.MODIFIER_STATE_INVISIBLE) then return true end
+  if NPC.HasModifier(myHero, "modifier_invoker_ghost_walk_self") then return true end
+  if NPC.HasAbility(myHero, "invoker_ghost_walk") then
+    if Ability.SecondsSinceLastUse(NPC.GetAbility(myHero, "invoker_ghost_walk")) > - 1 and Ability.SecondsSinceLastUse(NPC.GetAbility(myHero, "invoker_ghost_walk")) < 1 then
+      return true
+    end
+  end
+
+  if NPC.HasItem(myHero, "item_invis_sword", true) then
+    if Ability.SecondsSinceLastUse(NPC.GetItem(myHero, "item_invis_sword", true)) > - 1 and Ability.SecondsSinceLastUse(NPC.GetItem(myHero, "item_invis_sword", true)) < 1 then
+      return true
+    end
+  end
+  if NPC.HasItem(myHero, "item_silver_edge", true) then
+    if Ability.SecondsSinceLastUse(NPC.GetItem(myHero, "item_silver_edge", true)) > - 1 and Ability.SecondsSinceLastUse(NPC.GetItem(myHero, "item_silver_edge", true)) < 1 then
+      return true
+    end
+  end
+
+  return false
 end
 
 return wtfBrist
